b124c99109ccb1063ae70e297b7dfa5aa4aec116
Superllb93 <longbao.lv@gmail.com>
2019-10-11 19:12:18 +0800

    add CookieAuthentication project

---
+++ CookieAuthentication/CookieAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Security.Claims;
+using CookieAuthentication.Models.Account;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CookieAuthentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AccountController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if ("admin" == model.Account && "123qwe" == model.Password)
+            {
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                identity.AddClaim(new Claim(ClaimTypes.Name, model.Account));
+
+                var principal = new ClaimsPrincipal(identity);
+                HttpContext.SignOutAsync();
+
+                if (model.RememberMe)
+                {
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = model.RememberMe });
+                }
+                else
+                {
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties
+                    {
+                        IsPersistent = model.RememberMe,
+                        ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(int.Parse(_configuration.GetValue<string>("DefaultCookieExpireTimeInMinutes") ?? "30"))
+                    });
+                }
+
+                return RedirectToLocal(returnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError("", "账号不存在");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult LogOff()
+        {
+            HttpContext.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ CookieAuthentication/CookieAuthentication/Models/Account/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CookieAuthentication.Models.Account
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "用户名不能为空")]
+        public string Account { get; set; }
+
+        [Required(ErrorMessage = "密码不能为空")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
