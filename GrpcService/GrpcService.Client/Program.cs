eb74acdfad68fb3bfa8bfebc51945e063bf6ef37
Superllb93 <longbao.lv@gmail.com>
2019-10-14 17:01:23 +0800

    bi-directional streaming call

--- GrpcService/GrpcService.Client/Program.cs
+++ GrpcService/GrpcService.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using System;
 using System.Threading.Tasks;
 
@@ -13,6 +14,35 @@ namespace GrpcService.Client
             var reply = await client.SayHelloAsync(
                 new HelloRequest { Name = "longbao" });
             Console.WriteLine("Greeter 服务返回数据: " + reply.Message);
+
+            Console.WriteLine("------双向流式调用开始------");
+            var printClient = new Printer.PrinterClient(channel);
+            using (var call = printClient.Echo())
+            {
+                Console.WriteLine("Starting background task to receive messages");
+                var readTask = Task.Run(async () => 
+                {
+                    await foreach (var response in call.ResponseStream.ReadAllAsync())
+                    {
+                        Console.WriteLine(response.Message);
+                    }
+                });
+                Console.WriteLine("Starting to send messages");
+                Console.WriteLine("Type a message to echo then press enter.");
+                while (true)
+                {
+                    var result = Console.ReadLine();
+                    if (string.IsNullOrEmpty(result))
+                    {
+                        break;
+                    }
+                    await call.RequestStream.WriteAsync(new EchoMessageRequest { Message = result });
+                }
+                Console.WriteLine("Disconnecting");
+                await call.RequestStream.CompleteAsync();
+                await readTask;
+            }
+            Console.WriteLine("------双向流式调用结束------");
         }
     }
 }
---
+++ GrpcService/GrpcService.Server/Services/PrinterService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace GrpcService.Server.Services
+{
+    public class PrinterService : Printer.PrinterBase
+    {
+        public PrinterService()
+        {
+        }
+
+        public override async Task Echo(IAsyncStreamReader<EchoMessageRequest> requestStream, IServerStreamWriter<EchoMessageResponse> responseStream, ServerCallContext context)
+        {
+            var messageQueue = new Queue<string>();
+
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                messageQueue.Enqueue(request.Message);
+            }
+
+            while (messageQueue.TryDequeue(out var message))
+            {
+                await responseStream.WriteAsync(new EchoMessageResponse() { Message = $"收到的消息为：{message}" });
+
+                await Task.Delay(500);//此处主要是为了方便客户端能看出流调用的效果
+            }
+        }
+    }
+}
