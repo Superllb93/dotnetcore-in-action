e815f36dfe2d5a7f9e3268483f53e9aa99f532c5
Superllb93 <longbao.lv@gmail.com>
2019-10-14 14:27:09 +0800

    add FriendlyUrl project

---
+++ FriendlyUrl/FriendlyUrl/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FriendlyUrl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FriendlyUrl/FriendlyUrl/SlugifyParameterTransformer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System.Text.RegularExpressions;
+
+namespace FriendlyUrl
+{
+    public class SlugifyParameterTransformer : IOutboundParameterTransformer
+    {
+        public string TransformOutbound(object value)
+        {
+            return null == value
+                ? null
+                : Regex.Replace(value.ToString(), "([a-z])([A-Z])", "$1-$2").ToLower();
+        }
+    }
+}
