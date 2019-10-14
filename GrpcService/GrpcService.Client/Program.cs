079ceb54354a1c4759be4c21f9260f28cd59680a
Superllb93 <longbao.lv@gmail.com>
2019-10-14 15:19:19 +0800

    add GrpcService project

---
+++ GrpcService/GrpcService.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcService.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "longbao" });
+            Console.WriteLine("Greeter 服务返回数据: " + reply.Message);
+        }
+    }
+}
