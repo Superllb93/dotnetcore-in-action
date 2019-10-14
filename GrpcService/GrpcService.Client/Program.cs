de9a2cd743b1990e5ef395e8dc9c1a9e25a09185
Superllb93 <longbao.lv@gmail.com>
2019-10-14 17:10:41 +0800

    cancell stream

--- GrpcService/GrpcService.Client/Program.cs
+++ GrpcService/GrpcService.Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Grpc.Core;
 using Grpc.Net.Client;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GrpcService.Client
@@ -16,32 +17,51 @@ namespace GrpcService.Client
             Console.WriteLine("Greeter 服务返回数据: " + reply.Message);
 
             Console.WriteLine("------双向流式调用开始------");
+            var cts = new CancellationTokenSource();
+            //指定在2.5s后进行取消操作
+            cts.CancelAfter(TimeSpan.FromSeconds(2.5));
+
             var printClient = new Printer.PrinterClient(channel);
-            using (var call = printClient.Echo())
+            using (var call = printClient.Echo(cancellationToken: cts.Token))
             {
                 Console.WriteLine("Starting background task to receive messages");
                 var readTask = Task.Run(async () => 
                 {
-                    await foreach (var response in call.ResponseStream.ReadAllAsync())
+                    try
                     {
-                        Console.WriteLine(response.Message);
+                        await foreach (var response in call.ResponseStream.ReadAllAsync())
+                        {
+                            Console.WriteLine(response.Message);
+                        }
+                    }
+                    catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+                    {
+                        Console.WriteLine("Stream cancelled when print response.");
                     }
                 });
                 Console.WriteLine("Starting to send messages");
                 Console.WriteLine("Type a message to echo then press enter.");
                 while (true)
                 {
-                    var result = Console.ReadLine();
-                    if (string.IsNullOrEmpty(result))
+                    try
                     {
-                        break;
+                        var result = Console.ReadLine();
+                        if (string.IsNullOrEmpty(result))
+                        {
+                            break;
+                        }
+                        await call.RequestStream.WriteAsync(new EchoMessageRequest { Message = result });
                     }
-                    await call.RequestStream.WriteAsync(new EchoMessageRequest { Message = result });
+                    catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+                    {
+                        Console.WriteLine("Stream cancelled when send request.");
+                    }
                 }
                 Console.WriteLine("Disconnecting");
                 await call.RequestStream.CompleteAsync();
                 await readTask;
             }
+            cts.Dispose();
             Console.WriteLine("------双向流式调用结束------");
         }
     }
